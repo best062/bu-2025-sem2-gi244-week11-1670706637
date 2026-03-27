24b0c7bda503821ffe0b69090570370c8a6f24f2
best062 <jantaramanee99@gmail.com>
2026-03-27 16:48:33 +0700

    Inclass

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public Transform FocalPoint;
 
     private Rigidbody rb;
+    public bool hasPowerUp = false;
 
     private InputAction moveAction;
     private InputAction smashAction;
@@ -25,6 +27,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var move = moveAction.ReadValue<Vector2>();
+        rb.AddForce(move.y * speed * FocalPoint.forward);
+        if (breakAction.IsPressed())
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (hasPowerUp == true)
+            {
+                var rb = collision.gameObject.GetComponent<Rigidbody>();
+                var dir = collision.transform.position - transform.position;
+                rb.AddForce(100 * dir.normalized, ForceMode.Impulse);
+            }
+        }
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            if (CountdownCoroutine != null)
+            {
+                StopCoroutine(CountdownCoroutine);
+            }
+            CountdownCoroutine = StartCoroutine(PowerUpCountdown());
+        }
+    }
+    
+    private Coroutine CountdownCoroutine;
+
+    IEnumerator PowerUpCountdown()
+    {
+        yield return new WaitForSeconds(10f);
+        hasPowerUp = false;
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -7,7 +8,40 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
-
+        InvokeRepeating(nameof(RandomSpawn),0,1f);
+        StartCoroutine(Hello());
+        StartCoroutine(Goodbye());
+        StartCoroutine(SpawnRoutine());
     }
 
+    void RandomSpawn()
+    {
+        var index = Random.Range(0, spawnPoints.Length);
+        var spawnPoint = spawnPoints[index];
+        Instantiate(enemyPrefab,spawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator Hello()
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Hello" + Time.frameCount);
+    }
+
+    IEnumerator Goodbye()
+    {
+        yield return null; 
+        Debug.Log("Goodbye" + Time.frameCount + "          " + Time.time);
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Goodbye" + Time.frameCount + "          " + Time.time);
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        yield return new WaitForSeconds(5f);
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3f);
+        }
+    }
 }
